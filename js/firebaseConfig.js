b1f529167ca1b8de85f8aa189018197d9bae65c0
krantikadam2601-collab <kratikadam2601@gmail.com>
2026-03-29 18:43:51 +0530

    Firebase setup completed

---
+++ js/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDfsS49KS5G_KJqH05HqSBucZSQ_0KflI0",
+  authDomain: "bookbazaar-234be.firebaseapp.com",
+  projectId: "bookbazaar-234be",
+  storageBucket: "bookbazaar-234be.firebasestorage.app",
+  messagingSenderId: "953407819480",
+  appId: "1:953407819480:web:96d2cd17f8dcd8df2011ad"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
